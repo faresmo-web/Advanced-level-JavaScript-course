4ecf409bc92ac44d289b915ff522ef1c2fcd6752
faresmo-web <sm3305358@gmail.com>
2025-02-13 00:51:53 +0200

    JS

--- The first challenge/script.js
+++ The first challenge/script.js
@@ -1,34 +1,42 @@
 function getPosts(userId){
-    let requst = new XMLHttpRequest()
-    requst.open("GET", "https://jsonplaceholder.typicode.com/posts?userId="+userId)
-    requst.responseType = "json"
-    requst.send()
-    requst.onload = function(){
-        if(requst.status >= 200 && requst.status < 300){
-            let posts = requst.response
-            for(post of posts){
-                let content = `
-                    <div class="post" id="post">
-                        <h3>${post.title}</h3>
-                        <h4>${post.body}</h4>
-                    </div>
-                `
-                document.getElementById("posts").innerHTML += content
-            }
-        }else(
-            alert("error")
-        )
-    }
+
+    
+
+    fetch("https://jsonplaceholder.typicode.com/posts?userId="+userId)
+    .then(response=>{
+        if(response.ok){
+            return response.json()
+        }
+    })
+    .then(posts => {
+        document.getElementById("posts").innerHTML = ""
+        for(post of posts){
+            let content = `
+                <div class="post" id="post">
+                    <h3>${post.title}</h3>
+                    <h4>${post.body}</h4>
+                </div>
+            `
+            document.getElementById("posts").innerHTML += content
+        }
+    
+    })
+
 }
 
 function getUsers(){
-    let requst = new XMLHttpRequest()
-    requst.open("GET", "https://jsonplaceholder.typicode.com/users")
-    requst.responseType = "json"
-    requst.send()
-    requst.onload = function(){
-        if(requst.status >= 200 && requst.status < 300){
-            let users = requst.response
+    return new Promise((resolve, reject)=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>{
+            if(response.ok){
+                return response.json()
+            }
+            else{
+                reject("Error with users request")
+            }
+        })
+        .then(users => {
+            document.getElementById("users").innerHTML = ""
             for(user of users){
                 let content = `
                     <div id="user" class="user" onclick="userClicked(${user.id}, this)">
@@ -38,14 +46,20 @@ function getUsers(){
                 `
                 document.getElementById("users").innerHTML += content
             }
-        }else(
-            alert("error")
-        )
-    }
-}
-getPosts(1)
-getUsers()
+            resolve()
+        
+        })
+    })
+    
 
+}
+getUsers()
+.then(()=>{
+    getPosts(1)
+})
+.catch((error)=>{
+    console.log(error)
+})
 
 function userClicked(id, el){
     getPosts(id)
